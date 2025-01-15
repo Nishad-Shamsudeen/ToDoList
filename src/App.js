195f96ab4ce0863371747724f1a3b63f3cca7fe1
Nishad Shamsudeen <nishadshmsudeen@gmail.com>
2025-01-15 20:45:13 +0530

    Initiall commit before deployment to GitHub

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [toDoInput,inputState]=useState('')
+  const [toDo,toDoState]=useState([])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDoInput} onChange={(e)=>inputState(e.target.value)} type= "text" placeholder="🖊️ Add item..." />
+        <i className="fas fa-plus" onClick={()=>toDoState([...toDo,{id:Date.now(),text:toDoInput,status:false}])}></i>
+      </div>
+    
+    { toDo.map((object,index)=>{
+
+   return (
+      <div className="todos" key={index}>
+        <div className="todo">
+          <div className="left">
+            <input onChange={(e)=>{
+              console.log(e.target.checked)
+              console.log(object)
+              toDoState(toDo.filter(object2=> {
+                if(object.id===object2.id)
+                {
+                  object2.status=e.target.checked
+                }
+                return object2
+                  
+              }))
+            }} value={object.status}type="checkbox" name="" id="" />
+            
+           <p>{object.text}</p>
+          </div>
+          <div className="right">
+            <i className="fas fa-times"></i>
+          </div>
+        </div>
+      </div>   )
+   } ) }
+   {toDo.map((object)=>{
+   if(object.status){
+    return (<h1>{object.text}</h1>)
+   }
+  })}
     </div>
   );
 }
