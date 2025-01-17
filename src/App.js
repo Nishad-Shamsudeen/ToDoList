a1c16b5c7db478e9c557bf187fec6ca2d8099c89
nishad-shamsudeen <nishadshmsudeen@gmail.com>
2025-01-17 22:00:22 +0530

    Updated Theame and Correct checkbox bug

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [toDoInput,inputState]=useState('')
-  const [toDo,toDoState]=useState([])
-  const [date, setDate] = useState(new Date());
-  const today=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday"]
-
+  const [toDoInput, inputState] = useState('')
+  const [toDo, toDoState] = useState([])
+  const date = new Date();
+  const today = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+  
   //var glob=false;
   return (
     <div className="app">
@@ -16,63 +16,65 @@ function App() {
       </div>
       <div className="subHeading">
         <br />
-        <h2>Yeah !!! Its ...{today[date.getDay()]}🌝 ☕</h2>
-        
+        <h2>Yeah !!! Its ...{today[date.getDay()]}</h2>
+
       </div>
       <div className="input">
-        <input value={toDoInput} onChange={(e)=>inputState(e.target.value)} type= "text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus" onClick={()=>toDoState([...toDo,{id:Date.now(),text:toDoInput,status:false}])}></i>
+        <input value={toDoInput} onChange={(e) => inputState(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i className="fas fa-plus"
+          onClick={() => {
+            toDoState([...toDo, { id: Date.now(), text: toDoInput, status: false }]);
+            inputState("")
+          }}></i>
       </div>
-    
-    { 
-    toDo.map((object,index)=>{
-      {       
 
-   return (
-      <div className="todos" key={index}>
-        <div className="todo">
-          <div className="left">
-            <input onChange={(e)=>{
-              
-              toDoState(toDo.filter((object2 => {
-                if(object.id===object2.id)
-                {
-                  object2.status=e.target.checked
-                  return true;
-                 // console.log(object2)
-                }
-                return false;
-                  
-              })))
-            }} value={object.status}type="checkbox" name="" id="" />
-            
-           <p>{object.text}</p>
-          </div>
-          <div className="right">
-            <i className="fas fa-times" onClick={()=>{
+      {
+        toDo.map((object, index) => {
+          {
 
-              toDoState(toDo.filter(object3=>
-              {
-                if(object.id===object3.id)
-                  {
-                  return false;
-                   
-                  }
-                  return true;
-              }
-              ))}}></i>
-          </div>
-        </div>
-      </div>  
-       )
-    
-      
-    }
-   } ) 
-  
-   } 
+            return (
+              <div className="todos" key={index}>
+                <div className="todo">
+                  <div className="left">
+                    <input onChange={(e) => {
 
-  {/*toDo.map((obj)=>{
+                      toDoState(toDo.filter((object2 => {
+                        if (object.id === object2.id) {
+                          object2.status = e.target.checked
+                          //return true;
+                          // console.log(object2)
+                        }
+                        return object2;
+
+                      })))
+                    }} value={object.status} type="checkbox" name="" id="" />
+
+                    <p>{object.text}</p>
+                  </div>
+                  <div className="right">
+                    <i className="fas fa-times" onClick={() => {
+
+                      toDoState(toDo.filter(object3 => {
+                        if (object.id === object3.id) {
+                          return false;
+
+                        }
+                        return true;
+                      }
+                      ))
+                    }}></i>
+                  </div>
+                </div>
+              </div>
+            )
+
+
+          }
+        })
+
+      }
+
+      {/*toDo.map((obj)=>{
   
   if(obj.status===true)
   {
@@ -80,7 +82,7 @@ function App() {
    
     
  } )*/}
-   
+
     </div>
   );
 }
